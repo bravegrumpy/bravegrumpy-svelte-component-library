c65c6fff5454a4f144c1467cb8b3201e5f40219d
Robin Zollner <code@bravegrumpy.com>
2025-05-09 15:09:57 -0400

    adding a copy of the css file to lib and exporting it

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,5 +3,6 @@ import Logo from "./Logo.svelte";
 import NavButton from "./NavButton.svelte";
 import Navigation from "./Navigation.svelte";
 import ThemeToggle from "./ThemeToggle.svelte";
+import * as styles from "./app.css"
 
-export { Logo, NavButton, Navigation, ThemeToggle }
+export { Logo, NavButton, Navigation, ThemeToggle, styles }
