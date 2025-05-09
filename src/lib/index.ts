e3ee032c21565ddc313fb06545db99975d107ab1
Robin Zollner <code@bravegrumpy.com>
2025-05-09 14:56:07 -0400

    adding component exports

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,7 @@
 // Reexport your entry components here
+import Logo from "./Logo.svelte";
+import NavButton from "./NavButton.svelte";
+import Navigation from "./Navigation.svelte";
+import ThemeToggle from "./ThemeToggle.svelte";
+
+export { Logo, NavButton, Navigation, ThemeToggle }
