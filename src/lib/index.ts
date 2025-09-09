85bbae8032104ede2c4c3214f57add2d822617fe
Robin Zollner <code@bravegrumpy.com>
2025-09-09 02:39:22 -0400

    exporting navigation type ref in package

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,9 +1,17 @@
 // Reexport your entry components here
 import Logo from './Logo.svelte';
 import NavButton from './NavButton.svelte';
-import Navigation from './Navigation.svelte';
+import Navigation, { type ref} from './Navigation.svelte';
 import ThemeToggle from './ThemeToggle.svelte';
 import Button from './Button.svelte';
 import PageTitle from './PageTitle.svelte';
 
-export { Logo, NavButton, Navigation, ThemeToggle, Button, PageTitle };
+export { 
+    Logo, 
+    NavButton, 
+    Navigation, 
+    type ref,
+    ThemeToggle, 
+    Button, 
+    PageTitle 
+};
