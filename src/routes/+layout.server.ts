a5a395ce613023ab3f22fe8d1a933fdf66326f62
Robin Zollner <code@bravegrumpy.com>
2025-09-11 13:51:33 -0400

    aligning with prettier format rules

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,14 +1,14 @@
-import type { LayoutServerLoad } from "./$types.js"
-import type { ref } from "$lib/Navigation.svelte"
+import type { LayoutServerLoad } from './$types.js';
+import type { ref } from '$lib/Navigation.svelte';
 
 export function load<LayoutServerLoad>() {
-    const routes:ref[] = [
-		{ id: 'home', href: '/', text: 'Home'},
-		{ id: 'about', href: '/about', text: 'About'},
-		{ id: 'experiments', href: '/experiments', text: 'Playground'}
+	const routes: ref[] = [
+		{ id: 'home', href: '/', text: 'Home' },
+		{ id: 'about', href: '/about', text: 'About' },
+		{ id: 'experiments', href: '/experiments', text: 'Playground' }
 	];
 
-    return {
-        routes
-    }
+	return {
+		routes
+	};
 }
