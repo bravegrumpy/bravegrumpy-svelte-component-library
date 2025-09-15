e22c98599d13a69c60d947c77679b7ef37cb9246
Robin Zollner <code@bravegrumpy.com>
2025-09-15 00:38:40 -0400

    adding class prop to Main component

--- src/lib/Main.svelte
+++ src/lib/Main.svelte
@@ -5,9 +5,9 @@
 	interface Props extends SvelteMainElement {
 		children?: Snippet;
 	}
-	let { children }: Props = $props();
+	let { children, class: mainClasses= '' }: Props = $props();
 </script>
 
-<main class="bg:grid bg:grid-cols-[grid-template-columns:1fr_2fr_1fr] bg:gap-4 bg:m-5">
+<main class="bg:grid bg:grid-cols-[grid-template-columns:1fr_2fr_1fr] bg:gap-4 {mainClasses}">
 	{@render children?.()}
 </main>
--- src/routes/experiments/two/+page.svelte
+++ src/routes/experiments/two/+page.svelte
@@ -2,6 +2,18 @@
 	import Section from '$lib/Section.svelte';
 </script>
 
-<Section class="bg:col-start-2 bg:w-full">
+<Section class="bg:col-start-1 bg:col-end-4">
 	<p>Hello World</p>
 </Section>
+
+<div class="bg:col-span-1 bg:col-start-1 bg:bg-bravegrumpy-accent2a bg:rounded bg:dark:bg-bravegrumpy-black bg:text-bravegrumpy-black bg:dark:text-bravegrumpy-accent2a bg:border-2 bg:border-solid bg:py-2">
+    <p>Hello World</p>
+</div>
+
+<div class="bg:col-span-1 bg:col-start-2 bg:bg-bravegrumpy-accent2a bg:rounded bg:dark:bg-bravegrumpy-black bg:text-bravegrumpy-black bg:dark:text-bravegrumpy-accent2a bg:border-2 bg:border-solid bg:py-2">
+    <p>Hello World</p>
+</div>
+
+<div class="bg:col-span-1 bg:col-start-3 bg:bg-bravegrumpy-accent2a bg:rounded bg:dark:bg-bravegrumpy-black bg:text-bravegrumpy-black bg:dark:text-bravegrumpy-accent2a bg:border-2 bg:border-solid bg:py-2">
+    <p>Hello World</p>
+</div>
