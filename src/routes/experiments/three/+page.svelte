25269a8ebc1ea41d2db320e2785b6e74b5bd57e8
Robin Zollner <code@bravegrumpy.com>
2025-09-24 16:39:58 -0400

    updating pallette

--- src/routes/experiments/three/+page.svelte
+++ src/routes/experiments/three/+page.svelte
@@ -1,28 +1,141 @@
+<script lang="ts">
+	import Section from "$lib/Section.svelte";
+</script>
+	<Section>
+		<h2>Completed Colors</h2>
+	</Section>
+<div class="bg:w-full bg:flex bg:flex-row bg:flex-wrap">
+	<div class="bg:w-1/3">
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-royal-purple-50"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-royal-purple-100"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-royal-purple-200"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-royal-purple-300"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-royal-purple-400"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-royal-purple-500"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-royal-purple-600"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-royal-purple-700"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-royal-purple-800"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-royal-purple-900"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-royal-purple-950"></div>
+	</div>
+	<div class="bg:w-1/3">
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-tradewind-50"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-tradewind-100"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-tradewind-200"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-tradewind-300"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-tradewind-400"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-tradewind-500"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-tradewind-600"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-tradewind-700"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-tradewind-800"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-tradewind-900"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-tradewind-950"></div>
+	</div>
+	<div class="bg:w-1/3">
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-cerulean-blue-50"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-cerulean-blue-100"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-cerulean-blue-200"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-cerulean-blue-300"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-cerulean-blue-400"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-cerulean-blue-500"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-cerulean-blue-600"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-cerulean-blue-700"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-cerulean-blue-800"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-cerulean-blue-900"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-cerulean-blue-950"></div>
+	</div>
+	<div class="bg:w-1/3">
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-apple-50"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-apple-100"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-apple-200"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-apple-300"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-apple-400"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-apple-500"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-apple-600"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-apple-700"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-apple-800"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-apple-900"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-apple-950"></div>
+	</div>
+	<div class="bg:w-1/3">
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-flush-orange-50"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-flush-orange-100"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-flush-orange-200"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-flush-orange-300"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-flush-orange-400"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-flush-orange-500"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-flush-orange-600"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-flush-orange-700"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-flush-orange-800"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-flush-orange-900"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-flush-orange-950"></div>
+	</div>
+	<div class="bg:w-1/3">
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-affair-50"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-affair-100"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-affair-200"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-affair-300"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-affair-400"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-affair-500"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-affair-600"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-affair-700"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-affair-800"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-affair-900"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-affair-950"></div>
+	</div>
+	<div class="bg:w-1/3">
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-fog-50"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-fog-100"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-fog-200"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-fog-300"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-fog-400"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-fog-500"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-fog-600"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-fog-700"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-fog-800"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-fog-900"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-fog-950"></div>
+	</div>
+	<div class="bg:w-1/3">
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-melrose-50"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-melrose-100"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-melrose-200"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-melrose-300"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-melrose-400"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-melrose-500"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-melrose-600"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-melrose-700"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-melrose-800"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-melrose-900"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-melrose-950"></div>
+	</div>
+</div>
+	<h2 class="bg:font-heading bg:text-center bg:text-3xl bg:bg-bravegrumpy-accent2a bg:dark:bg-bravegrumpy-black bg:border-2 bg:border-solid bg:rounded bg:w-fit bg:mx-auto bg:px-10 bg:py-2 bg:my-2">Current Color</h2>
 <div class="bg:w-full bg:flex bg:flex-row bg:flex-wrap">
 	<div class="bg:w-1/2">
-		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-affairA-50"></div>
-		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-affairA-100"></div>
-		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-affairA-200"></div>
-		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-affairA-300"></div>
-		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-affairA-400"></div>
-		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-affairA-500"></div>
-		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-affairA-600"></div>
-		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-affairA-700"></div>
-		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-affairA-800"></div>
-		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-affairA-900"></div>
-		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-affairA-950"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-hawkes-blueA-50"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-hawkes-blueA-100"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-hawkes-blueA-200"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-hawkes-blueA-300"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-hawkes-blueA-400"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-hawkes-blueA-500"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-hawkes-blueA-600"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-hawkes-blueA-700"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-hawkes-blueA-800"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-hawkes-blueA-900"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-hawkes-blueA-950"></div>
 	</div>
 	<div class="bg:w-1/2">
-		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-affairB-50"></div>
-		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-affairB-100"></div>
-		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-affairB-200"></div>
-		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-affairB-300"></div>
-		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-affairB-400"></div>
-		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-affairB-500"></div>
-		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-affairB-600"></div>
-		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-affairB-700"></div>
-		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-affairB-800"></div>
-		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-affairB-900"></div>
-		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-affairB-950"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-hawkes-blueB-50"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-hawkes-blueB-100"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-hawkes-blueB-200"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-hawkes-blueB-300"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-hawkes-blueB-400"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-hawkes-blueB-500"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-hawkes-blueB-600"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-hawkes-blueB-700"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-hawkes-blueB-800"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-hawkes-blueB-900"></div>
+		<div class="bg:col-span-4 bg:w-full bg:h-15 bg:bg-hawkes-blueB-950"></div>
 	</div>
 </div>
